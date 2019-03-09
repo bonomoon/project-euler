43f407bd6c345a3ec9cd605d4d1a56c98b3288da
bonomoon <moonbonoz@gmail.com>
2019-03-09 23:57:27 +0900

    solved: problem 0006

---
+++ p0006-Sum_square_difference/README.md
@@ -0,0 +1,11 @@
+# Problem 0006 - Sum square difference
+## 1. Problem
+___The sum of the squares of the first ten natural numbers is,___
+![problem_1](../img/p0006/p0006_problem1.PNG)
+___The square of the sum of the first ten natural numbers is,___
+![problem_2](../img/p0006/p0006_problem2.PNG)
+___Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.___
+
+___Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.___
+
+## 2. Solution
--- p0006-Sum_square_difference/p0006.cpp
+++ p0006-Sum_square_difference/p0006.cpp
@@ -1,8 +1,39 @@
 #include <cstdio>
 
+#define ll long long
+#define MOD 1000000007
+
+// n(n+1)(2n+1)/6
+// pow(
+
+ll fast_pow(ll a, ll b) {
+    if(b == 0) return 1;
+
+    ll tmp = fast_pow(a, b/2);
+
+    if(b % 2 == 0) {
+        return (tmp * tmp) % MOD; 
+    } else {
+        return ((((tmp * tmp) % MOD)) * a) % MOD;
+    }
+}
+
+inline int sum_of_square(int n) {
+    return n*(n + 1)*(2*n + 1) / 6;
+}
+
+inline ll square_of_sum(int n) {
+    int sum = n*(n + 1) / 2;
+
+    return fast_pow(sum, 2);
+}
+
 int main () {
+    const int n = 100;
 
+    ll res = square_of_sum(n) - sum_of_square(n);
 
+    printf("%lld\n", res); 
 
     return 0;
 }
